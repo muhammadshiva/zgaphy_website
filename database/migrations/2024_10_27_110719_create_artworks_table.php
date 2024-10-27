3456c6219d86ab0fbc0592e12429510b8c1b9d1f
shivamuhammad <shiva@venturology.id>
2024-10-27 23:47:57 +0700

    create artwork migration

---
+++ database/migrations/2024_10_27_110719_create_artworks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('artworks', function (Blueprint $table) {
+            $table->id();
+            $table->string('artwork_code');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('cover')->nullable();
+            $table->text('description');
+            $table->unsignedInteger('price')->default(0);
+            $table->string('series')->nullable();
+            $table->unsignedInteger('frame_width')->default(0);
+            $table->unsignedInteger('frame_height')->default(0);
+            $table->foreignId('category_id')->constrained('categories')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('artworks');
+    }
+};
