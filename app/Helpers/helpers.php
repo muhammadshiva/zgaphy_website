e79a0c6d3f1b417e67604b97016eb582430cb9af
shivamuhammad <shiva@venturology.id>
2024-10-28 19:29:10 +0700

    create database helper & username generator

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\User;
+
+if (!function_exists('flashMessage')) {
+    function flashMessage($message, $type = 'success'): void
+    {
+        session()->flash('message', $message);
+        session()->flash('type', $type);
+    }
+}
+
+if (!function_exists('usernameGenerator')) {
+    function usernameGenerator($name): string
+    {
+        $username = strtolower(preg_replace('/\+/', '_', trim($name)));
+        $original_username = $username;
+
+        $count = 1;
+
+        while (User::where('username', $username)->exists()) {
+            $username = $original_username . $count;
+            $count++;
+        }
+
+        return $username;
+    }
+}
