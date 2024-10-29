339faede6c47b54b940ca0dcc7928776682faaeb
shivamuhammad <shiva@venturology.id>
2024-10-29 08:49:01 +0700

    adjustment navlink

--- resources/js/Components/NavLink.jsx
+++ resources/js/Components/NavLink.jsx
@@ -9,7 +9,7 @@ export default function NavLink({active = false, url = '#', title, icon: Icon, .
             href={url}
 
             className={cn(
-                active ? 'bg-gradient-to-r from-orange-400 via-orange-600 to-orange-500 font-semibold text-white hover:text-white' : 'text-muted-foreground hover:text-orange-500',
+                active ? 'bg-gradient-to-r from-orange-400 via-orange-500 to-orange-500 font-semibold text-white hover:text-white' : 'text-muted-foreground hover:text-orange-500',
                 'flex items-center gap-3 rounded-lg font-medium transition-all'
             )}
         >
