7dbaf056587b1a09901426f0017e6cea31a6b09b
shivamuhammad <shiva@venturology.id>
2024-10-29 11:11:20 +0700

    feat(utils): fine payment status

--- resources/js/lib/utils.js
+++ resources/js/lib/utils.js
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
+
+
+export const FINEPAYMENTSTATUS = {
+    PENDING: 'Tertunda',
+    SUCCESS: 'Sukses',
+    FAILED: 'Gagal',
+}
+
+export default function flashMessage(params){
+    return params.props.flash_message;
+}
