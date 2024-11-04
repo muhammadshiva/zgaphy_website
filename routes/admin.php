efd471610263f966608d8f7ef6e5b44dc408d265
shivamuhammad <shiva@venturology.id>
2024-11-04 11:00:50 +0700

    feat(auth): artwork routes

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ArtworkController;
 use App\Http\Controllers\Admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,5 +13,14 @@ Route::middleware(['auth'])->prefix('admin')->group(function () {
         Route::get('categories/edit/{category}', 'edit')->name('admin.categories.edit');
         Route::put('categories/edit/{category}', 'update')->name('admin.categories.update');
         Route::delete('categories/destroy/{category}', 'destroy')->name('admin.categories.destroy');
+    });
+
+    Route::controller(ArtworkController::class)->group(function () {
+        Route::get('artworks', 'index')->name('admin.artworks.index');
+        Route::get('artworks/create', 'create')->name('admin.artworks.create');
+        Route::post('artworks/create', 'store')->name('admin.artworks.store');
+        Route::get('artworks/edit/{artwork}', 'edit')->name('admin.artworks.edit');
+        Route::put('artworks/edit/{artwork}', 'update')->name('admin.artworks.update');
+        Route::delete('artworks/destroy/{artwork}', 'destroy')->name('admin.artworks.destroy');
     });
 });
